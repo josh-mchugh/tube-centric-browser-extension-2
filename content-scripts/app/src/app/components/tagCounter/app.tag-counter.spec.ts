625d6e0aeccd5570b2b455a81bab4191592077fa
jmchugh <joshua.mchugh01@gmail.com>
2019-12-31 16:00:31 -0500

    TC-42 - Fixed bug where the total tags being used was incorrect and exceeding the total tag count

--- content-scripts/app/src/app/components/tagCounter/app.tag-counter.spec.ts
+++ content-scripts/app/src/app/components/tagCounter/app.tag-counter.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { TagCounterComponent } from './app.tag-counter';
 
-describe('AppComponent', () => {
+describe('TagCounter', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -14,18 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(TagCounterComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'content-scripts'`, () => {
-    const fixture = TestBed.createComponent(TagCounterComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('content-scripts');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(TagCounterComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('content-scripts app is running!');
   });
 });
--- content-scripts/app/src/app/components/tagCounter/services/app-tag-counter.service.spec.ts
+++ content-scripts/app/src/app/components/tagCounter/services/app-tag-counter.service.spec.ts
@@ -1,12 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AppAppTagCounterServiceCounterService } from './app-tag-counter.service';
+import { AppTagCounterService } from './app-tag-counter.service';
 
 describe('AppCounterServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: AppCounterService = TestBed.get(AppTagCounterService);
+    const service: AppTagCounterService = TestBed.get(AppTagCounterService);
     expect(service).toBeTruthy();
+  });
+
+  it('Tags Count should be length of tags array', () => {
+    const service: AppTagCounterService = TestBed.get(AppTagCounterService);
+    service.setTags(['test1', 'test2', 'test3', 'test10', 'test11']);
+    expect(service.getTagsCount()).toBe(5);
+  });
+
+  it('Tags in title length should be 3', () => {
+    const service: AppTagCounterService = TestBed.get(AppTagCounterService);
+    service.setTags(['test1', 'test2', 'test10']);
+    service.setTitle('test1 test2 test10');
+    expect(service.getTitleTagsCount()).toBe(3);
+  });
+
+  it('Tags in description length should 3', () => {
+    const service: AppTagCounterService = TestBed.get(AppTagCounterService);
+    service.setTags(['test1', 'test2', 'test10']);
+    service.setDescription('test1 test2 test10');
+    expect(service.getDescriptionTagsCount()).toBe(3);
+  });
+
+  it('Tags total should be 4', () => {
+    const service: AppTagCounterService = TestBed.get(AppTagCounterService);
+    service.setTags(['test1', 'test2', 'test10', 'test11']);
+    service.setTitle('test1 test2 test10');
+    service.setDescription('test1 test2 test10 test11');
+    expect(service.getTotalTagsUsedCount()).toBe(4);
   });
 });
