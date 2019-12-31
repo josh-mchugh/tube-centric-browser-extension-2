4cd3bcff9dfc5b6351161030258e822418d761de
Josh McHugh <joshua.mchugh01@gmail.com>
2019-12-31 19:48:48 +0000

    TC-41 - Tag Count loads correctly when url navigation occurs

--- content-scripts/src/main.js
+++ content-scripts/src/main.js
@@ -1,36 +1,65 @@
 var $ = require("jquery");
 var browser = require("webextension-polyfill");
+var UriTemplate = require("uri-template");
 
 console.log("Hello, content script here.");
 
 // Initialize the CSS styles for the content script
 $(function() {
-    const link = $("<link>")
-      .attr("rel", "stylesheet")
-      .attr("href", browser.runtime.getURL("/content-scripts/app/styles.css"));
-    $("head").append(link);
-    const app = $("<script>")
-        .attr("type", "text/javascript")
-        .attr("src", browser.runtime.getURL("/content-scripts/app/app.js"));
-    $("head").append(app);
+  const link = $("<link>")
+    .attr("rel", "stylesheet")
+    .attr("href", browser.runtime.getURL("/content-scripts/app/styles.css"));
+  $("head").append(link);
 });
 
-// Observe the DOM until the element is present
-var observer = new MutationObserver(function(mutations) {
-    
-  const leftContainer = $("#left");
-  if ($(leftContainer).length) {
-    
-    if($("#tcAppVideoMeta").length === 0) {
-      const appComponent = $("<app-tag-counter>");
-      $(leftContainer).prepend(appComponent);
-    }
-    this.disconnect();
+// Initialize the app when the page is loaded the url matches
+$(function() {
+  if(isVideoEditUrl(window.location.pathname)) {
+    initTagCounter();
   }
 });
 
-// Start observing
-observer.observe(document.body, { //document.body is node target to observe
-    childList: true, //This is a must have for the observer with subtree
-    subtree: true //Set to true if changes must also be observed in descendants.
+// URL Event Listener
+browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // listen for messages sent from background.js
+  if (request.message === 'URL_CHANGE') {
+    console.log(JSON.stringify(request));
+    const pathname = new URL(request.url).pathname;
+    if(isVideoEditUrl(pathname)) {
+      initTagCounter();
+      const uriInfo = UriTemplate.extract("/video/{videoId}/edit", pathname);
+      console.log(uriInfo.videoId);
+    }
+  }
 });
+
+function initTagCounter() {
+  
+  var observer = new MutationObserver(function(mutations) {
+    
+    mutations.forEach(mutation => {
+      if($(mutation.target).attr("id") === "left") {
+
+        const component = $("<app-tag-counter>");
+        $(mutation.target).prepend(component);
+        
+        const componentScript = $("<script>")
+          .attr("type", "text/javascript")
+          .attr("src", browser.runtime.getURL("/content-scripts/app/app.js"));
+        componentScript.insertAfter(component);
+  
+        this.disconnect();
+
+      }
+    });
+  });
+
+  observer.observe(document.body, {
+      childList: true,
+      subtree: true
+  });
+}
+
+function isVideoEditUrl(pathname) {
+  return new RegExp(/\/video\/[\S]*\/edit/i).test(pathname);
+}
